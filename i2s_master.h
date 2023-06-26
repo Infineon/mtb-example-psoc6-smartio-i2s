8461d69eafd979d39840596693f7dec3a57cfc7b
gitlab-runner <do-not-reply@cypress.com>
2023-06-26 10:35:05 -0500

    Upload mtb-example-psoc6-smartio-i2s [1465]

--- i2s_master.h
+++ i2s_master.h
@@ -5,7 +5,7 @@
 *              the I2S Master implementation using the SPI and Smart I/O.
 *
 *******************************************************************************
-* Copyright 2020-2022, Cypress Semiconductor Corporation (an Infineon company) or
+* Copyright 2020-2023, Cypress Semiconductor Corporation (an Infineon company) or
 * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
 *
 * This software, including source code, documentation and related
